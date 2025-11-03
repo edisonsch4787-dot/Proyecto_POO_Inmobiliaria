ba9ee1265b37c777747c11ce13a40e687db49129
Jefferson1256 <jeffsaav@espol.edu.ec>
2025-11-02 23:39:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         int op;
+        Agencia agencia = new Agencia();
+        agencia.setNombre("Mi Agencia");
         do {
             Scanner sc=new Scanner(System.in);
             System.out.println("""
@@ -32,25 +34,87 @@ public class Main {
             
             switch(op){
                 case 1://Pendiente revisar
-                    String op1="2";
                     while(!"N".equals(op1)){
-                        Persona p1=new Cliente(0,"nombre","cedula","telf");
-                        System.out.println("Ingrese el nombre del cliente");
-                        String nombre=sc.nextLine();
-                        System.out.println("Ingrese numero de cedula");
-                        String Ci=sc.nextLine();
-                        System.out.println("Ingrese su numero de telefono ");
-                        String tel=sc.nextLine();
-                        p1.setNombre(nombre);
-                        p1.setCi(Ci);
-                        ((Cliente)p1).setTelf(tel);
-                        System.out.println(((Cliente)p1).toString());
-                        System.out.println("Desea seguir registrando? (S/N)");
-                        op1=sc.nextLine().toUpperCase();
+                        
+                        String nombre = "";
+                        String ci = "";
+                        String tel = "";
+                        while(nombre.length() == 0){  
+                            System.out.println("Ingrese el nombre del cliente");
+                            nombre=sc.nextLine();
+                            if(nombre.length() == 0){   
+                                System.out.println("Debe ingresar un nombre.");
+                            }
+                        }
+                        while(Ci.length() == 0){
+                            System.out.println("Ingrese numero de cedula");                            
+                            Ci=sc.nextLine();
+                            if(Ci.length() == 0){
+                                System.out.println("Ingrese numero de cedula");
+                             }
+                         }
+                        while(tel.length() == 0){
+                            
+                            System.out.println("Ingrese su numero de telefono ");                            
+                            tel=sc.nextLine();
+                            if(tel.length() == 0){
+                                
+                                System.out.println("Ingrese su numero de telefono ");
+                            }
+                        }
+                        
+                        Cliente p1=new Cliente(0,"nombre","cedula");
+                        
+                        agencia.registrarCliente(p1);
+                        System.out.println("Cliente registrado: " + p1.toString());
+                        System.out.println("¿Desea seguir registrando? (S/N)");
+                        op1 = sc.nextLine().toUpperCase();
                     }
                     
                     break;
                 case 2:
+                    String op2 = "S";
+                    while(!op2.equalsIgnoreCase("N")) {
+                        String nombre = "";
+                        String ci = "";
+                        String idAgente = "";
+                        while(nombre.length() == 0){
+                            System.out.println("Ingrese el nombre del agente:");
+                            nombre = sc.nextLine();
+                            if(nombre.length() == 0){
+                                System.out.println("Debe ingresar un nombre.");
+                            }
+                        }
+
+                        while(ci.length() == 0){
+                            System.out.println("Ingrese número de cédula:");
+                            ci = sc.nextLine();
+                            if(ci.length() == 0){
+                                System.out.println("Debe ingresar un número de cédula.");
+                            }
+                        }
+
+                        while(idAgente.length() == 0){
+                            System.out.println("Ingrese ID del agente:");
+                            idAgente = sc.nextLine();
+                            if(idAgente.length() == 0){
+                                System.out.println("Debe ingresar un ID.");
+                            }
+                        }
+                        AgenteInmobiliario agente = new AgenteInmobiliario();
+                        agente.setIdAgente(idAgente);
+                        agente.setNombre(nombre);
+                        agente.setCi(ci);
+                        agente.setComisionAcumulada(0.0);
+                        agente.setPropiedadesAsignadas(new ArrayList<>());
+
+                        agencia.registrarAgente(agente);
+
+                        System.out.println("Agente registrado: " + agente.toString());
+
+                        System.out.println("¿Desea seguir registrando? (S/N)");
+                        op2 = sc.nextLine().toUpperCase();
+                    }
                     break;
                 case 3:
                     break;
